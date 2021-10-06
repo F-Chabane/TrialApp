1a69de65132dda81359f10cf2153bbfe02b750af
Fathi Chabane <fathi.chabane@aptiv.com>
2021-10-06 12:55:59 +0200

    Initial Commit

---
+++ src/TrialApp/TrialApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrialApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
